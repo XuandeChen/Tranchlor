1b08f89823a08dec3838b02a4367873e9e72f352
shawnd1031 <shawnd1031@gmail.com>
2020-07-30 16:24:25 -0400

    Commit Xuande

--- FunctionsXC.vb
+++ FunctionsXC.vb
@@ -161,13 +161,184 @@ Module FunctionsXC
         f = phi ^ (4 / 3) * (1 - S) ^ (10 / 3)
         Return f
     End Function
-    'isotherm function 
+    '' Isotherm function 
     'need to analyse and complet Xuande 2020.07.20
-    Public Function GetHtoS(ByRef H As Double)
-        Dim S As Double
-        S = H
+    'isotherm functions from David C. 2020.07.30 W(H)
+    Public Function Wat(ByVal H As Decimal, ByVal Hol As Decimal, ByRef T As Decimal, ByRef Tijd As Decimal, ByRef tPro As Single, ByRef Vct As Single, ByRef Nct As Single, ByRef EsC As Single, ByRef Wsat As Single, ByRef Hydr As Single, ByRef ciment As Single, ByRef Wol As Boolean) As Decimal
+        Dim Time As Decimal
+        Dim f1 As Decimal
+        Dim f2 As Decimal
+        Dim f3 As Decimal
+        Dim f4 As Decimal
+        Dim f5 As Decimal
+        Dim f6 As Decimal
+        Dim f7 As Decimal
+        Dim f8 As Decimal
+        Dim f9 As Decimal
+        Dim f10 As Decimal
+        Dim f11 As Decimal
+        Dim f12 As Decimal
+        Dim f13 As Decimal
+        Dim Para(3, 3) As Decimal
+        Dim b(3) As Decimal
+        Dim a(3) As Decimal
+        Dim wcOld, wcNew, wNew As Decimal
+        Dim i As Short
+        Dim j As Short
+        Dim tst01 As Boolean = False
+        Dim VH As Single
+        Dim VHol As Single
+        Time = Tijd + CDec(tPro)
+        VH = CInt(H * 1000) / 1000
+        VHol = CInt(Hol * 1000) / 1000
+        H = CInt(H * 100) / 100
+        Hol = CInt(Hol * 100) / 100
+        If T < -100 Then T = -100
+        If T > 100 Then T = 100
+        If System.Math.Abs(H - Hol) = 0.01 And System.Math.Abs(VH - VHol) < 0.02 Then Hol = H 'court-circuite les petites valeurs proche 0.05
+        If H = Hol Then
+            If Wol = True Then
+                H = H - 0.1
+            Else
+                H = H + 0.1
+            End If
+            tst01 = True
+        End If
+        If H <= Hol Then
+            If tst01 = True Then
+                H = H + 0.1
+                tst01 = False
+            End If
+            Wol = True
+            ' On the desorption branch
+            f1 = 0.125                                                          'c1
+            f2 = 0.173 - 0.431 * ((T - 20) / 25)                                'c2
+            f3 = 0.06 + 0.392 * ((T - 20) / 25)                                 'c3
+            f4 = 0.17 + (0.035 + 0.029 * (EsC - 0.4) / 0.15) * ((T - 20) / 25)  'c4
+
+            f5 = 0.389 - 26 * EsC / 15 + 4.4 * EsC ^ 2 - 8 * EsC ^ 3 / 3        'we
+            f6 = 1 - 0.161 * Hydr                                               'ht
+
+            Para(1, 1) = f6 ^ 2             'para1
+            Para(1, 2) = 1 - 2 * f6         'para2
+            Para(1, 3) = Para(1, 1) - f6    'para3
+            Para(2, 1) = -2 * f6            'para4
+            Para(2, 2) = -Para(2, 1)        'para5
+            Para(2, 3) = 1 - Para(1, 1)     'para6
+            Para(3, 1) = 1                  'para7
+            Para(3, 2) = -1                 'para8
+            Para(3, 3) = f6 - 1             'para9
+
+            b(1) = EsC - f4 * Hydr                          'b1
+            b(2) = (f1 + f2 * f6 + f3 * f6 ^ 2) * Hydr      'b2
+            b(3) = (f2 + 2 * f3 * f6) * Hydr                'b3
+
+            For i = 1 To 3
+                a(i) = 0
+            Next
+
+            For i = 1 To 3
+                For j = 1 To 3
+                    a(i) = a(i) + Para(i, j) * b(j)
+                Next
+            Next
+
+            f7 = 1 / (1 - f6) ^ 2
+            For i = 1 To 3
+                a(i) = a(i) * f7         'a, b, c
+            Next
+            f8 = 0                                                  'A
+            f9 = Hydr / 0.35 * (-f1 / 0.35 + 0.35 * f3)             'C
+            f10 = (f2 + 0.7 * f3) * Hydr - 0.7 * f9                 'B
+            f11 = (a(1) + a(2) * 1.0 + a(3) * 1.0 ^ 2) * ciment     'wm
+            f11 = ciment * Wsat / f11
+
+            Select Case Hol
+                Case Is <= 0.35
+                    wcOld = (f8 + f10 * Hol + f9 * Hol ^ 2) * f11
+                Case Is < f6
+                    wcOld = (f1 + f2 * Hol + f3 * Hol ^ 2) * Hydr * f11
+                Case Else
+                    wcOld = (a(1) + a(2) * Hol + a(3) * Hol ^ 2) * f11
+            End Select
+            Select Case H
+                Case Is <= 0.35
+                    wcNew = (f8 + f10 * H + f9 * H ^ 2) * f11
+                Case Is < f6
+                    wcNew = (f1 + f2 * H + f3 * H ^ 2) * Hydr * f11
+                Case Else
+                    wcNew = (a(1) + a(2) * H + a(3) * H ^ 2) * f11
+            End Select
+
+            If wcOld > Wsat Then wcOld = Wsat
+            If wcNew > Wsat Then wcNew = Wsat
+            If wcOld < 0 Then wcOld = 0
+            If wcNew < 0 Then wcNew = 0
+
+            Wat = wcNew
+
+        Else
+            If tst01 = True Then
+                H = H - 0.1
+                tst01 = False
+            End If
+            Wol = False
+            ' On the sorption branche
+            If Time < CDec(5) Then      'calcul de Vt(t) correstpondant à f2 et Nt(t) = f3
+                f2 = CDec(0.024)
+                f3 = CDec(5.5)
+            Else
+                f2 = CDec(0.068) - CDec(0.22) / Time
+                f3 = CDec(2.5) + CDec(15) / Time
+            End If
+            f2 = f2 * CDec(Vct) * (CDec(0.85) + CDec(0.45) * CDec(EsC)) * CDec(1.0) ' calcul de vm
+            f3 = f3 * (CDec(0.33) + CDec(2.2) * CDec(EsC)) * CDec(Nct) * CDec(1.0) ' calcul de n
+            f4 = CDec(System.Math.Exp(855 / (273.16 + T))) ' calcul de C
+            f5 = ((CDec(1) - CDec(1) / f3) * f4 - CDec(1)) / (f4 - CDec(1)) ' calcul de k
+            f6 = ciment * (1 + EsC)
+            f9 = f6 * f4 * f5 * f2 * CDec(1.0) / ((CDec(1) - f5 * CDec(1.0)) * (CDec(1) + (f4 - CDec(1)) * f5 * CDec(1.0))) 'calcul de wmax
+            wcOld = f6 * f4 * f5 * f2 * Hol / ((CDec(1) - f5 * Hol) * (CDec(1) + (f4 - CDec(1)) * f5 * Hol))
+            wcNew = f6 * f4 * f5 * f2 * H / ((CDec(1) - f5 * H) * (CDec(1) + (f4 - CDec(1)) * f5 * H))
+            wcOld = wcOld * Wsat / f9
+            wcNew = wcNew * Wsat / f9
+
+            If wcOld > Wsat Then wcOld = Wsat
+            If wcNew > Wsat Then wcNew = Wsat
+            If wcOld < 0 Then wcOld = 0
+            If wcNew < 0 Then wcNew = 0
+
+            Wat = wcNew
+
+        End If
+
+        'If CDbl(H * 1000) <= CDbl(Hol * 1000) Then Water = Wol
+        If Wat > Wsat Then Wat = Wsat
+        If Wat < 0 Then Wat = 0
+        Hol = H
+    End Function
+    'isotherm functions modified from David C. and reformed by Xuande C. 2020.07.30 S(H)
+    Public Function GetHtoS(ByRef H As Double, ByRef type As Integer, ByRef W_C_ratio As Double, ByRef T As Double, ByRef Day As Double, ByRef rho_l As Double, ByRef rho_c As Double)
+        Dim VT As Double = 1
+        Dim NT As Double = 1
+        Dim C0 As Double = 855
+
+        ' Adsorption BET model [Bazant]
+        Dim C As Double = Cfunc(C0, T)
+        Dim Vm As Double = Vmfunc(Day, W_C_ratio, type, VT)
+        Dim n As Double = nfunc(Day, W_C_ratio, type, NT)
+        Dim k As Double = kfunc(C, n)
+        Dim wc1 As Double = (C * k * Vm * 1) / ((1 - k * 1) * (1 + (C - 1) * k * 1))
+        Dim phi As Double = wc1 * (rho_c / rho_l)
+        Dim wc As Double = (C * k * Vm * H) / ((1 - k * H) * (1 + (C - 1) * k * H))
+        Dim S As Double = (wc / phi) * (rho_c / rho_l)
+        'S = H
         Return S
     End Function
+    Public Function Cfunc(ByRef C0 As Double, ByRef T As Integer)
+        Dim C As Double = Math.Exp((C0 / T))
+        Return C
+    End Function
+
     'get average of humidity on an element 
     Public Function GetAvgH(ByRef He() As Double)
         Dim H_avg As Double
